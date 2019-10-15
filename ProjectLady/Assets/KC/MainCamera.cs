23f07e3b382ec73136afcd2d2c3346cf50995384
KEIc046 <huishis74@gmail.com>
2019-10-15 11:42:07 +0900

    試しのカメラの動きを追加

---
+++ ProjectLady/Assets/KC/MainCamera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    public GameObject _player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(_player.transform.position.x, 5, -10); //キャラにカメラの追従処理
+
+        if (transform.position.x < 0)
+        {
+            transform.position = new Vector3(0, 5, -10);
+        }
+
+        if (transform.position.x >= 18)
+        {
+            transform.position = new Vector3(18, 5, -10);
+        }
+    }
+}
