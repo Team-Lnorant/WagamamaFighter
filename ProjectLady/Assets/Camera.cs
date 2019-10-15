f95819d1aa13bf73c789469eb1b3b89bcabe659d
Pninari <saitokosei3411@gmail.com>
2019-10-15 17:49:47 +0900

    Unity お嬢様の動き、歩き・止まりを追加しました。

---
+++ ProjectLady/Assets/Camera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+
+    private GameObject _lady;
+
+    void Start()
+    {
+        _lady = GameObject.Find("Lady");
+    }
+
+    void Update()
+    {
+        Vector3 _ladyPos = this._lady.transform.position;
+        transform.position = new Vector3(
+            _ladyPos.x, _ladyPos.y, transform.position.z);
+    }
+}
---
+++ ProjectLady/Assets/Icon.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Icon : MonoBehaviour
+{
+    public Sprite[] _iconTbl;
+
+    public enum EventIcon{
+        QUESTION,
+        TIRED,
+        THIRSTY,
+        EXCLAMATION
+    };
+
+
+}
